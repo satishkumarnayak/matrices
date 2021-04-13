855b3d3e2c0ad69ca743f3739ed00d94b9dfb407
Satish Nayak <satish_nayak@epam.com>
2021-04-13 19:28:51 +0530

    Matrix Binary Search

--- src/main/java/com/instanceofcake/BinarySearchMatrix.java
+++ src/main/java/com/instanceofcake/BinarySearchMatrix.java
@@ -1,10 +1,63 @@
 package com.instanceofcake;
 
+import java.util.Arrays;
+
 public class BinarySearchMatrix {
 
   public boolean search(int[][] input, int target) {
+    boolean result = false;
+    int low = 0;
+    int high = input.length;
+    int mid = low + high / 2;
+    int output = 99;
+    while (low < high && mid < high) {
 
-    return false;
+      output = binarySearch2(input[mid], 0, input[mid].length - 1, target);
+      if(output == 0) {
+        result = true;
+        break;
+      }
+        
+
+      else if(output == -1) {
+        
+        high = mid;
+        mid = low + high /2;
+        
+      } 
+    
+      else if(output == 1) {
+        low = mid;
+        mid = low + high /2; 
+       
+      }
+     
+     
+    }
+
+    return result;
+  }
+
+  public int binarySearch2(int[] array, int start, int end, int target) {
+    System.out.println(Arrays.toString(array));
+    int mid = start + end / 2;
+    int result = 99;
+    if (start < end) {
+      if (target == array[mid]) {
+        result = 0;
+      } else if (target < array[mid]) {
+        end = mid;
+        result = binarySearch2(array, start, end, target);
+        if(result != 0)
+          result = -1;
+      } else if (target > array[mid]) {
+        start = mid;
+        result = binarySearch2(array, start, end, target);
+        if(result != 0)
+          result = 1;
+      }
+    }
+    return result;
   }
 
 
--- src/test/java/com/instanceofcake/BinarySearchMatrixTest.java
+++ src/test/java/com/instanceofcake/BinarySearchMatrixTest.java
@@ -13,23 +13,35 @@ public class BinarySearchMatrixTest {
     binarySearch = new BinarySearchMatrix();
   }
 
-  // @Test
+  @Test
   public void testBinarySearch() {
-    int[][] input = {{1, 2, 5, 7}, {8, 9, 10, 12}, {15, 20, 25, 26}};
+    int[][] input = {{1, 2, 5, 7},
+                    {8, 9, 10, 12},
+                     {15, 20, 25, 26}};
+
 
     assertTrue(binarySearch.search(input, 9));
+    assertTrue(binarySearch.search(input, 8));
+    assertTrue(binarySearch.search(input, 10));
+    assertTrue(binarySearch.search(input, 12));
+    assertTrue(binarySearch.search(input, 1));
+    assertTrue(binarySearch.search(input, 15));
+
+    assertFalse(binarySearch.search(input, 27));
+    assertFalse(binarySearch.search(input, 0));
+
   }
 
-  @Test
+  // @Test
   public void testBS() {
     int[] input = {1, 2, 5, 7};
-    
-    assertTrue(binarySearch.binarySearch(input, 0,3,2));
-    assertTrue(binarySearch.binarySearch(input, 0,3,1));
-    assertTrue(binarySearch.binarySearch(input, 0,3,5));
-    assertTrue(binarySearch.binarySearch(input, 0,3,7));
-    assertFalse(binarySearch.binarySearch(input, 0,3,8));
-    assertFalse(binarySearch.binarySearch(input, 0,3,-1));
+
+    assertTrue(binarySearch.binarySearch(input, 0, 3, 2));
+    assertTrue(binarySearch.binarySearch(input, 0, 3, 1));
+    assertTrue(binarySearch.binarySearch(input, 0, 3, 5));
+    assertTrue(binarySearch.binarySearch(input, 0, 3, 7));
+    assertFalse(binarySearch.binarySearch(input, 0, 3, 8));
+    assertFalse(binarySearch.binarySearch(input, 0, 3, -1));
   }
 
 
